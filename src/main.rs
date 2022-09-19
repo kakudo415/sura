89a6df53b0446981977f9bc5311dd857f9077c99
kakudo <kakudo415@gmail.com>
2022-09-20 00:23:17 +0900

    feat: Implement a screen with RAW mode in Linux.
    I used termios to achieve RAW mode in Linux like OS.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,106 @@
+use libc;
 use std::env;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::io::Write;
+use std::thread;
+use std::time;
 
 fn send_escape_sequence_csi(code: &str) {
     print!("\x1B[{}", code);
 }
 
-struct Editor {}
-
-impl Editor {
-    fn new() -> Self {
-        send_escape_sequence_csi("?1049h");
-        Editor {}
-    }
+#[cfg(any(target_os = "linux", target_os = "macos"))]
+struct Editor {
+    saved_term: libc::termios,
+    is_closing: bool,
 }
 
-impl Drop for Editor {
-    fn drop(&mut self) {
+#[cfg(target_os = "windows")]
+struct Editor {
+    is_closing: bool,
+}
+
+impl Editor {
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
+    fn new() -> Self {
+        send_escape_sequence_csi("?1049h");
+        let mut editor = Editor {
+            saved_term: libc::termios {
+                c_iflag: 0,
+                c_oflag: 0,
+                c_cflag: 0,
+                c_lflag: 0,
+                c_line: 0,
+                c_cc: [0u8; 32],
+                c_ispeed: 0,
+                c_ospeed: 0,
+            },
+            is_closing: false,
+        };
+        editor.enable_raw_mode();
+        send_escape_sequence_csi("1;1H");
+        editor
+    }
+
+    #[cfg(target_os = "windows")]
+    fn new() -> Self {
+        todo!()
+    }
+
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
+    fn close(&mut self) {
+        self.disable_raw_mode();
         send_escape_sequence_csi("?1049l");
+        self.is_closing = true;
+    }
+
+    #[cfg(target_os = "windows")]
+    fn close(&mut self) {
+        todo!()
+    }
+
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
+    fn enable_raw_mode(&mut self) {
+        unsafe {
+            let mut ptr = &mut self.saved_term;
+            libc::tcgetattr(0, ptr);
+        }
+        let mut termattr = self.saved_term;
+        termattr.c_lflag &= !(libc::ICANON | libc::ECHO | libc::ISIG);
+        termattr.c_cc[libc::VMIN] = 1;
+        termattr.c_cc[libc::VTIME] = 0;
+        unsafe {
+            libc::tcsetattr(0, libc::TCSANOW, &termattr);
+        }
+        unsafe {
+            libc::fcntl(0, libc::F_SETFL, libc::O_NONBLOCK);
+        }
+    }
+
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
+    fn disable_raw_mode(&self) {
+        unsafe {
+            libc::tcsetattr(0, libc::TCSANOW, &self.saved_term);
+        }
+    }
+
+    fn routine(&mut self) {
+        let mut buf = [0; 1];
+        let ptr = &mut buf;
+
+        let r = unsafe { libc::read(0, ptr.as_ptr() as *mut libc::c_void, 1) };
+        if r > 0 {
+            let c = char::from_u32((*ptr)[0]).unwrap();
+            print!("{}", c);
+            // Exit
+            if c == 'E' {
+                self.is_closing = true;
+            }
+        }
+        thread::sleep(time::Duration::from_millis(16));
+        std::io::stdout().flush().unwrap();
     }
 }
 
@@ -28,14 +110,14 @@ fn main() {
         panic!("ERROR: Select the input file");
     }
 
-    let file = File::open(&args[1]);
-    let reader = BufReader::new(file.unwrap());
+    let mut editor = Editor::new();
 
-    let editor = Editor::new();
-
-    for result in reader.lines() {
-        let line = result.unwrap();
-        println!("{}", line);
+    loop {
+        if editor.is_closing {
+            break;
+        }
+        editor.routine();
     }
-    std::thread::sleep(std::time::Duration::from_secs(3));
+
+    editor.close();
 }
