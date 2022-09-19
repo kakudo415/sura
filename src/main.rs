d7545c40512039e5605e59736e809b23a8d9a04c
kakudo <kakudo415@gmail.com>
2022-09-19 22:05:12 +0900

    feat: Implement a screen with alternative buffer
    It will shows 3 seconds.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,25 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
+fn send_escape_sequence_csi(code: &str) {
+    print!("\x1B[{}", code);
+}
+
+struct Editor {}
+
+impl Editor {
+    fn new() -> Self {
+        send_escape_sequence_csi("?1049h");
+        Editor {}
+    }
+}
+
+impl Drop for Editor {
+    fn drop(&mut self) {
+        send_escape_sequence_csi("?1049l");
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -12,14 +31,11 @@ fn main() {
     let file = File::open(&args[1]);
     let reader = BufReader::new(file.unwrap());
 
-    print!("\x1B[s");
-    print!("\x1B[2J");
-    print!("\x1B[1;1H");
+    let editor = Editor::new();
 
     for result in reader.lines() {
         let line = result.unwrap();
         println!("{}", line);
     }
-
-    print!("\x1B[u");
+    std::thread::sleep(std::time::Duration::from_secs(3));
 }
