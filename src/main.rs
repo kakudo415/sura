108cd9db1a8e58e6e0b9f0c400b436e441a9d74e
kakudo415 <kakudo415@gmail.com>
2022-09-19 18:11:24 +0900

    feat: Implement a screen which shows file content
    This is entry point.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("ERROR: Select the input file");
+    }
+
+    let file = File::open(&args[1]);
+    let reader = BufReader::new(file.unwrap());
+
+    print!("\x1B[s");
+    print!("\x1B[2J");
+    print!("\x1B[1;1H");
+
+    for result in reader.lines() {
+        let line = result.unwrap();
+        println!("{}", line);
+    }
+
+    print!("\x1B[u");
 }
