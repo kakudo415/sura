a185f89b800250ef14f27c6706ca46238fac1851
kakudo <kakudo415@gmail.com>
2022-09-21 04:08:17 +0900

    feat: Implement file editing but cannot go back
    Gap Buffer have not been implemented yet. So, currently simple Vec is being used.
    Backspace and Enter have not been implemented yet.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ mod terminal;
 
 use libc;
 use std::env;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
 use std::io::Write;
 use std::thread;
 use std::time;
@@ -9,14 +12,18 @@ use std::time;
 const ASCII_ESC: u32 = 27;
 
 struct Editor {
+    lines: Vec<String>,
     terminal: terminal::Terminal,
+    cursor: (usize, usize), // (line, column)
     is_closing: bool,
 }
 
 impl Editor {
     fn new() -> Self {
         Editor {
+            lines: Vec::new(),
             terminal: terminal::Terminal::open(),
+            cursor: (0, 0),
             is_closing: false,
         }
     }
@@ -25,15 +32,38 @@ impl Editor {
         let mut buf = [0; 1];
         let ptr = &mut buf;
 
-        let r = unsafe { libc::read(0, ptr.as_ptr() as *mut libc::c_void, 1) };
-        if r > 0 {
-            if (*ptr)[0] == ASCII_ESC {
-                self.is_closing = true;
-            }
-            let c = char::from_u32((*ptr)[0]).unwrap();
-            print!("{}", c);
+        if unsafe { libc::read(0, ptr.as_ptr() as *mut libc::c_void, 1) } <= 0 {
+            return;
         }
-        thread::sleep(time::Duration::from_millis(16));
+
+        match (*ptr)[0] {
+            ASCII_ESC => self.is_closing = true,
+            _ => {
+                let ch = char::from_u32((*ptr)[0]).unwrap();
+                self.insert(ch);
+            }
+        }
+
+        self.refresh();
+    }
+
+    fn insert(&mut self, ch: char) {
+        if self.lines.len() <= self.cursor.0 {
+            self.lines.resize(self.cursor.0 + 1, Default::default());
+        }
+        self.lines[self.cursor.0].insert(self.cursor.1, ch);
+        self.cursor.1 += 1;
+    }
+
+    fn refresh(&self) {
+        terminal::clear();
+        let mut row = 1;
+        for line in &self.lines {
+            terminal::move_cursor(row, 1);
+            print!("{}", line);
+            row += 1;
+        }
+        terminal::move_cursor(self.cursor.0 + 1, self.cursor.1 + 1);
         std::io::stdout().flush().unwrap();
     }
 }
@@ -46,11 +76,18 @@ fn main() {
 
     let mut editor = Editor::new();
 
+    for line in BufReader::new(File::open(&args[1]).unwrap()).lines() {
+        editor.lines.push(line.unwrap());
+    }
+    editor.refresh();
+    terminal::move_cursor(1, 1);
+
     loop {
         if editor.is_closing {
             break;
         }
         editor.routine();
+        thread::sleep(time::Duration::from_millis(1000 / 60));
     }
 
     editor.terminal.close();
--- src/terminal.rs
+++ src/terminal.rs
@@ -7,3 +7,11 @@ pub use windows::*;
 fn send_escape_sequence_csi(code: &str) {
     print!("\x1B[{}", code);
 }
+
+pub fn clear() {
+    send_escape_sequence_csi("2J");
+}
+
+pub fn move_cursor(row: usize, column: usize) {
+    send_escape_sequence_csi(format!("{};{}H", row, column).as_str());
+}
