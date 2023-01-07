f62d0e7e7e28190075c6be3f3c5df1729d69de9c
Kakudo Kentaro <31888089+kakudo415@users.noreply.github.com>
2023-01-08 05:26:00 +0900

    refactor: Get rid of unwrap()s

--- src/language.rs
+++ src/language.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use nix::unistd::{close, dup2, execv, fork, pipe, ForkResult};
 use serde_json::json;
 use std::collections::HashMap;
@@ -15,36 +16,43 @@ pub struct Client {
     unreturned: Arc<Mutex<HashMap<i32, oneshot::Sender<Response>>>>,
 }
 
-pub async fn initialize(path: String, event_sender: mpsc::UnboundedSender<Event>) -> Client {
-    let mut client = Client::new(path, event_sender).await;
-    let response = client
+pub async fn initialize(
+    path: String,
+    event_sender: mpsc::UnboundedSender<Event>,
+) -> Result<Client> {
+    let mut client = Client::new(path, event_sender).await?;
+    let _response = client
         .request(Request::new("initialize", json!({"capabilities": {}})))
-        .await;
+        .await?;
+    // PARSE RESPONSE (SUCH AS TO GET TRIGGER CHARACTERS)
     client
         .notify(Notification::new("initialized", json!({})))
-        .await;
-    client
+        .await?;
+    Ok(client)
 }
 
 impl Client {
-    pub async fn new(path: String, event_sender: mpsc::UnboundedSender<Event>) -> Client {
-        let response_pipe = pipe().unwrap();
-        let request_pipe = pipe().unwrap();
-        let path = CString::new(path).expect("CString::new failed");
+    pub async fn new(path: String, event_sender: mpsc::UnboundedSender<Event>) -> Result<Client> {
+        let response_pipe = pipe()?;
+        let request_pipe = pipe()?;
 
-        match unsafe { fork().unwrap() } {
+        match unsafe { fork()? } {
             ForkResult::Child => {
-                close(request_pipe.1).unwrap();
-                close(response_pipe.0).unwrap();
-                dup2(request_pipe.0, 0).unwrap();
-                dup2(response_pipe.1, 1).unwrap();
-                execv(&path, &[&path]).unwrap();
+                close(request_pipe.1)?;
+                close(response_pipe.0)?;
+                dup2(request_pipe.0, 0)?;
+                dup2(response_pipe.1, 1)?;
+
+                let path = CString::new(path).expect("CSTRING NEW FAILED");
+                execv(&path, &[&path])?;
+
                 panic!("UNEXPECTED SERVER TERMINATION")
             }
 
             ForkResult::Parent { child: _ } => {
-                close(request_pipe.0).unwrap();
-                close(response_pipe.1).unwrap();
+                close(request_pipe.0)?;
+                close(response_pipe.1)?;
+
                 let request_channel = unsafe { File::from_raw_fd(request_pipe.1) };
                 let response_channel = unsafe { File::from_raw_fd(response_pipe.0) };
                 let unreturned = Arc::new(Mutex::new(HashMap::new()));
@@ -55,40 +63,59 @@ impl Client {
                     unreturned.clone(),
                 ));
 
-                Client {
+                Ok(Client {
                     request_writer: BufWriter::new(request_channel),
                     unreturned,
-                }
+                })
             }
         }
     }
 
-    pub async fn request(&mut self, content: Request) -> Response {
+    pub async fn request(&mut self, content: Request) -> Result<Response> {
         let (response_sender, response_receiver) = oneshot::channel();
 
-        self.unreturned
-            .lock()
-            .unwrap()
-            .insert(content.id, response_sender);
+        match self.unreturned.lock() {
+            Ok(mut unreturned) => {
+                unreturned.insert(content.id, response_sender);
+            }
+            Err(err) => {
+                panic!("UNRETURNED REQUEST POOL LOCK FAILED\n{}", err);
+            }
+        }
 
-        let content = serde_json::to_vec(&content).unwrap();
-        self.send(content).await;
+        match serde_json::to_vec(&content) {
+            Ok(msg) => {
+                self.send(msg).await?;
+            }
+            Err(err) => {
+                panic!("NOTIFICATION SERIALIZE FAILED\n{}", err);
+            }
+        }
 
-        response_receiver.await.unwrap()
+        match response_receiver.await {
+            Ok(response) => Ok(response),
+            Err(err) => {
+                panic!("RESPONSE LISTENER HAS ALREADY CLOSED\n{}", err);
+            }
+        }
     }
 
-    pub async fn notify(&mut self, content: Notification) {
-        let content = serde_json::to_vec(&content).unwrap();
-        self.send(content).await;
+    pub async fn notify(&mut self, content: Notification) -> Result<()> {
+        match serde_json::to_vec(&content) {
+            Ok(msg) => {
+                self.send(msg).await?;
+            }
+            Err(err) => panic!("NOTIFICATION SERIALIZE FAILED\n{}", err),
+        }
+        Ok(())
     }
 
-    async fn send(&mut self, content: Vec<u8>) {
-        self.request_writer
-            .write(format!("Content-Length: {}\r\n\r\n", content.len()).as_bytes())
-            .await
-            .unwrap();
-        self.request_writer.write(&content).await.unwrap();
-        self.request_writer.flush().await.unwrap();
+    async fn send(&mut self, content: Vec<u8>) -> Result<()> {
+        let header = format!("Content-Length: {}\r\n\r\n", content.len());
+        self.request_writer.write(header.as_bytes()).await?;
+        self.request_writer.write(&content).await?;
+        self.request_writer.flush().await?;
+        Ok(())
     }
 }
 
@@ -96,46 +123,46 @@ async fn listen(
     mut response_reader: BufReader<File>,
     event_sender: mpsc::UnboundedSender<Event>,
     unreturned: Arc<Mutex<HashMap<i32, oneshot::Sender<Response>>>>,
-) {
+) -> Result<()> {
     loop {
-        let content = read_response(&mut response_reader).await;
-        let content: ServerMessage = serde_json::from_str(&content).unwrap();
-        match content {
-            ServerMessage::Response(response) => {
-                let response_id = response.id;
-                let sender: oneshot::Sender<Response> =
-                    unreturned.lock().unwrap().remove(&response_id).unwrap();
-                sender.send(response).unwrap();
-            }
+        let msg = read_response(&mut response_reader).await?;
+        match serde_json::from_slice::<ServerMessage>(&msg)? {
+            ServerMessage::Response(response) => match unreturned.lock() {
+                Ok(mut unreturned) => {
+                    if let Some(sender) = unreturned.remove(&response.id) {
+                        if let Err(_) = sender.send(response) {
+                            panic!("RESPONSE RECEIVER HAS ALREADY CLOSED");
+                        }
+                    } else {
+                        panic!("NO RESPONSE SENDER IN UNRETURNED REQUEST POOL");
+                    }
+                }
+                Err(_) => panic!("UNRETURNED REQUEST POOL LOCK FAILED"),
+            },
             ServerMessage::Notification(notification) => {
-                event_sender
-                    .send(Event::LanguageNotification(notification))
-                    .unwrap();
+                event_sender.send(Event::LanguageNotification(notification))?;
             }
         }
     }
 }
 
-async fn read_response(response_buffer: &mut BufReader<File>) -> String {
-    let content_length = read_response_header(response_buffer).await;
-    let mut buffer = vec![0; content_length];
-    response_buffer.read_exact(&mut buffer).await.unwrap();
-    std::str::from_utf8(&buffer).unwrap().to_string()
+async fn read_response(response_reader: &mut BufReader<File>) -> Result<Vec<u8>> {
+    let content_length = read_response_header(response_reader).await?;
+    let mut buf = vec![0; content_length];
+    response_reader.read_exact(&mut buf).await?;
+    Ok(buf)
 }
 
-async fn read_response_header(response_buffer: &mut BufReader<File>) -> usize {
+async fn read_response_header(response_reader: &mut BufReader<File>) -> Result<usize> {
     // TODO: handle Content-Type
     let mut content_length = 0;
-    let mut buffer = [0; 16]; // "Content-Length: "
-    response_buffer.read_exact(&mut buffer).await.unwrap();
-    loop {
-        response_buffer.read(&mut buffer[0..1]).await.unwrap();
-        if buffer[0] as char == '\r' {
-            response_buffer.read_exact(&mut buffer[0..3]).await.unwrap(); // "\n\r\n"
-            break;
-        }
-        let digit = buffer[0] - 48;
-        content_length = content_length * 10 + digit as usize;
+    let mut buf = [0; 16];
+    response_reader.read_exact(&mut buf).await?; // "Content-Length: "
+    response_reader.read(&mut buf[0..1]).await?;
+    while b'0' <= buf[0] && buf[0] <= b'9' {
+        content_length = content_length * 10 + (buf[0] - 48) as usize;
+        response_reader.read(&mut buf[0..1]).await?;
     }
-    content_length
+    response_reader.read_exact(&mut buf[0..3]).await?; // \n\r\n
+    Ok(content_length)
 }
